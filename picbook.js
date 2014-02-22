acf76402a823e46d9bd1d2123ce95cdc4885f466
Henrique Alves <henriquea@gmail.com>
2014-02-22 22:38:42 +0000

    Add basic DOM manipulation and stable this version

--- picbook.js
+++ picbook.js
@@ -1,3 +1,4 @@
+/* picbookJs 0.3.0 | Copyright (C) 2014 Henrique Alves, http://henriquea.github.io/picbookjs */
 var _picbookOptions = null;
 
 var picbookJs = (function(document){
@@ -5,10 +6,12 @@ var picbookJs = (function(document){
   'use strict';
 
   function picbookJs(options) {
+
     /*jslint validthis: true */
-    this.options = this._merge(this._options, options);
+    this.options = this.extend(this._options, options);
     this.init();
     _picbookOptions = this.options;
+
   }
 
   picbookJs.prototype = {
@@ -17,19 +20,33 @@ var picbookJs = (function(document){
 
     loaded: false,
 
+    dom: {},
+
     _options: {
       graph: 'https://graph.facebook.com/',
       limit: 10,
     },
 
     init: function() {
+
       this.loaded = false;
       this.getAlbum({
-        callback: 'picbookJs.prototype._albumComplete'
+        callback: 'picbookJs.prototype.albumComplete'
       });
+
+      // Setup DOM
+      this.dom.wrapper = document.querySelector('.picbook');
+      this.dom.title = document.createElement('h3');
+      this.dom.title.classList.add('album-title');
+      this.dom.photos = document.createElement('ul');
+      this.dom.photos.classList.add('photo-list');
+      this.dom.wrapper.appendChild(this.dom.title);
+      this.dom.wrapper.appendChild(this.dom.photos);
+
     },
 
     getAlbum: function(params) {
+
       var js = document.createElement('script'),
           options = _picbookOptions || this.options,
           url = options.graph + options.albumId,
@@ -41,10 +58,12 @@ var picbookJs = (function(document){
 
       s = document.getElementsByTagName('script')[0];
       s.parentNode.insertBefore(js, s.nextSibling);
+
     },
 
     // TODO: adpated the method for paging the data
     getPhotos: function(params) {
+
       var js = document.createElement('script'),
           options = _picbookOptions || this.options,
           url = options.graph + options.albumId + '/photos',
@@ -56,27 +75,34 @@ var picbookJs = (function(document){
 
       s = document.getElementsByTagName('script')[0];
       s.parentNode.insertBefore(js, s.nextSibling);
+
     },
 
-    _albumComplete: function(response) {
+    albumComplete: function(response) {
 
       var options = _picbookOptions || this.options;
+
       this.album = response;
       this.count = response.count;
       this.getPhotos({
-        callback: 'picbookJs.prototype._photosComplete'
+        callback: 'picbookJs.prototype.photosComplete'
       });
 
       if (typeof options.albumLoaded === 'function') {
         options.albumLoaded();
       }
+
+      this.dom.title.innerHTML = response.name;
+
     },
 
-    _photosComplete: function(response) {
+    photosComplete: function(response) {
 
       var options = _picbookOptions || this.options;
+
       this.photos = response.data;
       this.loaded = true;
+      this.createPhotos();
 
       if (typeof options.photosLoaded  === 'function') {
         options.photosLoaded();
@@ -84,13 +110,29 @@ var picbookJs = (function(document){
 
     },
 
-    _merge: function(a, b) {
+    createPhotos: function() {
+
+      var i, len, photo, li, imgHtml;
+      for (i=0, len = this.photos.length; i < len; i++) {
+        photo = this.photos[i];
+        li = document.createElement('li');
+        imgHtml = '<img src="image_path">';
+        li.classList.add('photo-list-item');
+        this.dom.photos.appendChild(li);
+        li.innerHTML = imgHtml.replace(/image_path/gi, this.photos[i].images[3].source);
+      }
+
+    },
+
+    extend: function(a, b) {
+
       for (var key in b) {
         if (b.hasOwnProperty(key)) {
           a[key] = b[key];
         }
       }
       return a;
+
     }
 
   };
