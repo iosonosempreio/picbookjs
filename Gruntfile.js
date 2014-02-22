0ef53cabae1472058ec1d2a2d784f6eac48df408
Henrique Alves <henriquea@gmail.com>
2014-02-22 22:38:01 +0000

    Update jshint options

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,8 +11,11 @@ module.exports = function (grunt) {
 
     jshint: {
       options: {
-        jshintrc: '.jshintrc',
-        reporter: require('jshint-stylish')
+        reporter: require('jshint-stylish'),
+        curly: true,
+        eqeqeq: true,
+        eqnull: true,
+        browser: true
       },
       gruntfile: {
         src: 'Gruntfile.js'
